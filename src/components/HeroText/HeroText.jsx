baafc395239421011ad6d0a835178d3f0810b64b
Jan Borowski <janekborowski809@gmail.com>
2024-09-12 17:06:13 +0200

    Small changes in component structure

--- src/components/HeroText/HeroText.jsx
+++ src/components/HeroText/HeroText.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import Header from "../Header/Header";
 import HeroButtons from "../HeroButtons/HeroButtons";
 import "./HeroText.scss";
 
 const HeroText = () => {
   return (
-    <div className="Hero__text">
-      <Header />
+    <>
+      
+      <div className="Hero__text">
+        <h1 className="Hero__text__title">
+          Zacznij pomagać!
+          <br />
+          Oddaj niechciane rzeczy w zaufane ręce
+        </h1>
+        <img
+          className="Hero__text__decoration"
+          src="src\assets\icons\Decoration.svg"
+          alt=""
+        />
 
-      <h1 className="Hero__text__title">
-        Zacznij pomagać!
-        <br />
-        Oddaj niechciane rzeczy w zaufane ręce
-      </h1>
-      <img
-        className="Hero__text__decoration"
-        src="src\assets\icons\Decoration.svg"
-        alt=""
-      />
-
-      <HeroButtons />
-    </div>
+        <HeroButtons />
+      </div>
+    </>
   );
 };
 
