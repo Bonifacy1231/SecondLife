115e250b37148fd594abf0bffc96df059311b610
Jan Borowski <janekborowski809@gmail.com>
2024-09-12 17:04:56 +0200

    Small changes in the structure of the component

--- src/Layout/Hero/Hero.jsx
+++ src/Layout/Hero/Hero.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import HeroText from "../../components/HeroText/HeroText";
 import "./Hero.scss";
 import HeroImg from "../../components/HeroImg/HeroImg";
+import Header from "../../components/Header/Header";
 
 const Hero = () => {
   return (
     <div className="Hero">
       <HeroImg />
-      <HeroText />
+      <div className="box">
+        <Header/>
+        <HeroText />
+      </div>
     </div>
   );
 };
