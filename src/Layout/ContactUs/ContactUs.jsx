bbbd6b66f17305ea63db85da37cac91d92085a39
Jan Borowski <janekborowski809@gmail.com>
2024-09-17 23:37:26 +0200

    Adding the ContactUs component. Adding basic structure and adding basic styling

---
+++ src/Layout/ContactUs/ContactUs.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./ContactUs.scss";
+
+const ContactUs = () => {
+  return (
+    <div className="ContactUs">
+      <div className="ContactUs__form">
+        <h2 className="ContactUs__form__title">Skontaktuj się z nami</h2>
+        <img
+          className="ContactUs__form__decoration"
+          src="src\assets\icons\Decoration.svg"
+          alt=""
+        />
+        <div className="ContactUs__form__box">
+          <div className="ContactUs__form__box__name form__box__single">
+            <label  htmlFor="">Wpisz swoje imię</label>
+            <input type="text" name="" id="" placeholder="Wpisz swoje imię..." />
+          </div>
+          <div className="ContactUs__form__box__email form__box__single">
+            <label htmlFor="">Wpisz swój email</label>
+            <input type="email" placeholder="Wpisz swój email..." />
+          </div>
+          <div className="ContactUs__form__box__message form__box__single">
+            <label htmlFor="">Wpisz swoją wiadomość</label>
+            <textarea name="" id="" placeholder="Wpisz swoją wiadomość..."></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
