1c139b5ce942130a5de91c4693c82ec7d8260fea
Jan Borowski <janekborowski809@gmail.com>
2024-09-22 00:46:05 +0200

    Creating the Login component. Adding styling for the section.

--- src/Layout/Login/Login.jsx
+++ src/Layout/Login/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Navigation from "../../components/Navigation/Navigation";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <Navigation />
+      <div className="login__container">
+        <div className="login__box">
+          <h2 className="login__box__title second-title">Zaloguj się</h2>
+          <img
+            className="login__box__decoration"
+            src="src\assets\icons\Decoration.svg"
+            alt=""
+          />
+          <div className="login__form">
+            <label htmlFor="">Email</label>
 
-export default Login
+            <input type="text" />
+
+            <label htmlFor="">Hasło</label>
+
+            <input type="password" />
+          </div>
+        </div>
+        <div>
+          <button>Załóż konto</button>
+          <button>Zaloguj się</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
