89a66decc9ee47a5b928abf56823200a2f6c8a36
Jan Borowski <janekborowski809@gmail.com>
2024-09-22 23:33:18 +0200

    Correction of tag names according to semantics and BEM. Correction of tags in relation to their placement. Addition of missing styling.

--- src/Layout/Login/Login.jsx
+++ src/Layout/Login/Login.jsx
@@ -3,34 +3,37 @@ import Navigation from "../../components/Navigation/Navigation";
 import "./login.scss";
 
 const Login = () => {
-  return (
-    <div className="login">
-      <Navigation />
-      <div className="login__container">
-        <div className="login__box">
-          <h2 className="login__box__title second-title">Zaloguj się</h2>
-          <img
-            className="login__box__decoration"
-            src="src\assets\icons\Decoration.svg"
-            alt=""
-          />
-          <div className="login__form">
-            <label htmlFor="">Email</label>
-
-            <input type="text" />
-
-            <label htmlFor="">Hasło</label>
-
-            <input type="password" />
-          </div>
-        </div>
-        <div>
-          <button>Załóż konto</button>
-          <button>Zaloguj się</button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<section className='login'>
+			<Navigation />
+			<div className='login__container'>
+				<div className='login__box'>
+					<h2 className='login__box__title second-title'>Zaloguj się</h2>
+					<img
+						className='login__box__decoration'
+						src='src/assets/icons/Decoration.svg'
+						alt='Dekoracja'
+					/>
+					<div className='login__form'>
+						<form className='login__form__fields'>
+							<div className='login__form__field'>
+								<label htmlFor='email'>Email</label>
+								<input type='text' id='email' />
+							</div>
+							<div className='login__form__field'>
+								<label htmlFor='password'>Hasło</label>
+								<input type='password' id='password' />
+							</div>
+						</form>
+						<div className='login__form__actions'>
+							<button type='button'>Załóż konto</button>
+							<button type='submit'>Zaloguj się</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Login;
